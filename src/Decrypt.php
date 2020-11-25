cacf996c633b45b0a180f9cdc13542aee5d43101
Tomas Tornevall <thorne@tornevall.net>
2020-11-25 16:30:35 +0100

    Prepared furthermore functions in classes

--- src/Decrypt.php
+++ src/Decrypt.php
@@ -4,5 +4,63 @@ namespace M3U8;
 
 class Decrypt
 {
+    private $mpDecryptBinary = '/usr/local/mp4decrypt/bin/mp4decrypt';
+    private $ffmpeg = '/usr/bin/ffmpeg';
+    private $wideVineKeys;
 
+    /**
+     * Set new binary for mp4decrypt.
+     * @param $binaryFile
+     * @return Decrypt
+     */
+    public function setDecryptBinary($binaryFile)
+    {
+        if (file_exists($binaryFile)) {
+            $this->mpDecryptBinary = $binaryFile;
+        }
+        return $this;
+    }
+
+    /**
+     * @param $binaryFile
+     * @return $this
+     */
+    public function setMpegLibrary($binaryFile)
+    {
+        if (file_exists($binaryFile)) {
+            $this->ffmpeg = $binaryFile;
+        }
+        return $this;
+    }
+
+    /**
+     * @param $keyString
+     * @return $this
+     */
+    public function setWideVineKeys($keyString)
+    {
+        $this->wideVineKeys = trim($keyString);
+
+        return $this;
+    }
+
+    /**
+     * @param $encryptedVideoFile
+     * @param $destinationVideoFile
+     * @return mixed
+     */
+    public function getDecryptedVideo($encryptedVideoFile, $destinationVideoFile)
+    {
+        $decodeApplication = sprintf(
+            "%s %s %s %s",
+            $this->mpDecryptBinary,
+            $this->wideVineKeys,
+            $encryptedVideoFile,
+            $destinationVideoFile
+        );
+
+        exec($decodeApplication, $output, $return);
+
+        return $return;
+    }
 }
--- src/Download.php
+++ src/Download.php
@@ -2,7 +2,48 @@
 
 namespace M3U8;
 
+use TorneLIB\Exception\ExceptionHandler;
+use TorneLIB\Module\Config\WrapperConfig;
+use TorneLIB\Module\Network\Wrappers\CurlWrapper;
+
 class Download
 {
+    private $wrapper;
 
+    /**
+     * User-Agent for 1080p.
+     * @var string
+     * @todo Need another user-agent since LG 24LF4820-BU is blacklisted.
+     */
+    private $browser1080 = 'Mozilla/5.0 (Web0S; Linux/SmartTV) AppleWebKit/538.2 (KHTML, like Gecko) Large Screen ' .
+    'Safari/538.2 LG Browser/7.00.00(LGE; 24LF4820-BU; 03.20.14; 1; DTV_W15L); webOS.TV-2015; LG NetCast.TV-2013 ' .
+    'Compatible (LGE, 24LF4820-BU, wireless)';
+
+    /**
+     * Download constructor.
+     */
+    public function __construct()
+    {
+        $this->wrapper = new CurlWrapper();
+        $config = new WrapperConfig();
+        $this->wrapper->setConfig($config);
+    }
+
+    /**
+     * Extract manifest as array.
+     * @param $manifestUrl
+     * @return array|mixed
+     * @throws ExceptionHandler
+     */
+    public function getPlaylistManifest($manifestUrl)
+    {
+        return explode("\n", $this->wrapper->request($manifestUrl)->getBody());
+    }
+
+    /**
+     * @param $manifestUrl
+     */
+    public function getVideoParts($manifestUrl) {
+
+    }
 }
