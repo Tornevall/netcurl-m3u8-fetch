96adb55811d2671e4625ae2591cfa3d04cb096bd
Tomas Tornevall <thorne@tornevall.net>
2020-11-27 20:01:29 +0100

    Make script run only merge with first argument.

--- download.php
+++ download.php
@@ -6,17 +6,21 @@ use M3U8\FileHandler;
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
+$mergeOnly = false;
+if ((isset($argv[1]) && $argv[1] === 'merge') || isset($argv[4])) {
+    $mergeOnly = true;
+}
+
 if (!isset($argv[3])) {
     printf("Usage: %s <video-playlist-url> <audio-playlist-url> <KID:IV>\n", $argv[0]);
     echo "KID:IV = Widevine decoded AES key (kidhash:ivhash).\n";
     echo "\n";
     exit;
 }
-
 $generalStoreDestination = __DIR__ . '/tmp';
 
 // Skip download again with arg4.
-if (!isset($argv[4])) {
+if (!$mergeOnly) {
     $downloader = new Download();
     $downloader
         ->setVideoManifest($argv[1])
